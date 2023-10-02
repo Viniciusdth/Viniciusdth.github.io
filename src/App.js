e3e9a2a9750a14a8cc3781ba50a52de795c61636
Vinicius Botelho <vi.f.botelho@gmail.com>
2023-10-02 00:48:52 -0300

    Base header

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import './App.css';
-
+import Header from './Components/Header/index';
+import Footer from './Components/Footer/index';
 function App() {
   return (
     <div className="App">
-      
+      <Header />
+      <div className=""> Sou o Conteúdo da página</div>
+      <Footer />
     </div>
   );
 }
